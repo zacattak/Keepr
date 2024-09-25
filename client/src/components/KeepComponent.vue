58361a9c414c01e80938b758df488b3200a60b06
Zac <step7476@alumni.uidaho.edu>
2024-09-25 17:17:24 -0600

    made adjustments to KeepComponent so that image is represented better and not so squished

--- client/src/components/KeepComponent.vue
+++ client/src/components/KeepComponent.vue
@@ -4,7 +4,7 @@
         <div @click="getKeepById()" class="selectable" type="button" data-bs-toggle="modal" data-bs-target="#keepModal">
             <p class="mb-0 text-center">Views:{{ keep.views }} Kept:{{ keep.kept }}</p>
             <h2 class="text-center">{{ keep.name }}</h2>
-            <img :src="keep.img" :alt="keep.name" class="img">
+            <img :src="keep.img" :alt="keep.name" class="img-fluid rounded shadow">
         </div>
         <div v-if="keep.creatorId == account.id">
 
