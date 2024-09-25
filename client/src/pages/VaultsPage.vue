9704853bd0a074412c27fe95ee5b140ad870fca3
Zac <step7476@alumni.uidaho.edu>
2024-09-25 17:24:18 -0600

    "My Vaults" page looking nice and clean after implementing masonry 🤘

--- client/src/pages/VaultsPage.vue
+++ client/src/pages/VaultsPage.vue
@@ -1,14 +1,22 @@
 <template>
 
-    <section class="container-fluid bg-info">
+    <div class="container-fluid bg-info">
 
-        <div v-for="vault in vaults" :key="vault.id" class="col-9 col-md-3 m-2 card mb-2 mt-2">
+        <section class="row">
 
-            <VaultComponent :vault="vault" />
+            <div class="masonry">
 
-        </div>
+                <div v-for="vault in vaults" :key="vault.id" class="">
 
-    </section>
+                    <VaultComponent :vault="vault" />
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </div>
 
 </template>
 
@@ -81,5 +89,37 @@ export default {
     border: 2px solid black;
     border-radius: 16px;
     box-shadow: 3px 3px 10px rgba(42, 6, 134, 0.31);
+}
+
+.masonry {
+    column-count: 3;
+    /* Set the number of columns */
+    column-gap: 1rem;
+    /* Adjust gap between columns */
+
+
+}
+
+.masonry>div {
+    break-inside: avoid;
+    /* Prevent the item from breaking between columns */
+    margin-bottom: 1rem;
+    /* Add some spacing between items */
+}
+
+/* For tablets (medium screens), show 2 columns */
+@media (max-width: 768px) {
+    .masonry {
+        column-count: 2;
+        /* Reduce to 2 columns */
+    }
+}
+
+/* For mobile phones (small screens), show 1 column */
+@media (max-width: 576px) {
+    .masonry {
+        column-count: 1;
+        /* Reduce to 1 column */
+    }
 }
 </style>
